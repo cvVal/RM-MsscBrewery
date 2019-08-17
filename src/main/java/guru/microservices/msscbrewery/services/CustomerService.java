533af8a327f36604319225a73a82e0fc3870ac9e
cvVal <cvvaldez_00@hotmail.com>
2019-08-17 17:30:47 -0500

    First commit

---
+++ src/main/java/guru/microservices/msscbrewery/services/CustomerService.java
@@ -0,0 +1,15 @@
+package guru.microservices.msscbrewery.services;
+
+import guru.microservices.msscbrewery.web.model.CustomerDto;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
+}
